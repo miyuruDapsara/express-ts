819361100c4bf75f7271f74d78d783a239d4c48f
miyuruDapsara <136076012+miyuruDapsara@users.noreply.github.com>
2025-08-25 01:27:35 +0530

    created route files

---
+++ src/routes/greeting.routes.ts
@@ -0,0 +1,13 @@
+import { Router, Request, Response, NextFunction } from "express";
+
+ const router: Router = Router();
+
+ router.get("/", (req: Request, res: Response, next: NextFunction) => { 
+    res.send("Hello World");
+ });
+
+ router.post("/", (req: Request, res: Response, next: NextFunction) => {
+    res.send("Hello World");
+ });
+
+ export default router;
