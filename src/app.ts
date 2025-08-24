eec6987e66e05f01097412f137940ee28ee5f948
miyuruDapsara <136076012+miyuruDapsara@users.noreply.github.com>
2025-08-25 01:25:37 +0530

    impliment routes

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 // Import express default AND types
 import express, { Application, Request, Response } from "express";
+import greetingroute from "./routes/greeting.routes";
+
 
 const app: Application = express(); // now express() is defined
 
@@ -8,6 +10,13 @@ app.use(express.json());
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world");
 });
+
+
+
+app.use("/greeting",greetingroute);
+
+
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
